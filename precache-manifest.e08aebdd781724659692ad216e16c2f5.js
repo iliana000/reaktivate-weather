8ffe6e602b1ff6c8ba47740923f7b3c9a28517dd
iliana <iliana000@gmail.com>
2020-03-12 15:27:56 +0200

    Updates

---
+++ precache-manifest.e08aebdd781724659692ad216e16c2f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fc8ca8fec098ac3e0a71767091735c",
+    "url": "/reaktivate-weather/index.html"
+  },
+  {
+    "revision": "fec30ba90e379112618d",
+    "url": "/reaktivate-weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81b2d892f36f3f8e2268",
+    "url": "/reaktivate-weather/static/js/2.df4c800b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reaktivate-weather/static/js/2.df4c800b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec30ba90e379112618d",
+    "url": "/reaktivate-weather/static/js/main.1518e860.chunk.js"
+  },
+  {
+    "revision": "f61ef22b4903a2932c5e",
+    "url": "/reaktivate-weather/static/js/runtime-main.2bb1464a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reaktivate-weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
